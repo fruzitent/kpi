cc98744994cddb161c4f769c45724827241537f7
fruzitent <fruzit@gmail.com>
2024-11-04 19:01:53 +0200

    feat(s3_sda/p01): generate file with random data

--- s3_dsa/p01/src/main.rs
+++ s3_dsa/p01/src/main.rs
@@ -1,5 +1,11 @@
 mod nacci;
 
 fn main() {
-    println!("Hello, world!");
+    let input_path = std::path::Path::new("./input.bin");
+    if !input_path.exists() {
+        let mut rng = nanorand::tls_rng();
+        let data: Vec<u8> = (0..2u32.pow(8)).map(|_| nanorand::Rng::generate(&mut rng)).collect();
+        let mut input_file = std::fs::File::create(&input_path).unwrap();
+        std::io::Write::write_all(&mut input_file, &data).unwrap();
+    }
 }
