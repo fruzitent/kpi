6d77dd564ff482e53156e4c51c39e6e1c50d545b
fruzitent <fruzit@gmail.com>
2024-11-19 02:48:14 +0200

    feat(s3_db/p02: create roles and users

---
+++ s3_db/p02/sql/00-initdb.sql
@@ -0,0 +1,29 @@
+-- roles
+drop role if exists viewer;
+create role viewer;
+
+drop role if exists editor;
+create role editor;
+grant viewer to editor with inherit true;
+
+drop role if exists owner;
+create role owner;
+grant editor to owner with inherit true;
+
+-- users
+drop user if exists employee_01;
+create user employee_01 with password 'pwd!01';
+grant editor to employee_01 with inherit true;
+
+drop user if exists employee_02;
+create user employee_02 with password 'pwd!02';
+grant viewer to employee_02 with inherit true;
+
+-- schema
+drop database if exists example;
+create database example;
+\connect example;
+
+drop schema if exists realtor;
+create schema realtor;
+set search_path to realtor;
