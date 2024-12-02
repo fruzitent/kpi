d3f2145476edd84323ab8042ea44785c888a9f69
fruzitent <fruzit@gmail.com>
2024-12-02 22:20:11 +0200

    feat(s3_db/p02): implement property table

--- s3_db/p02/sql/00-initdb.sql
+++ s3_db/p02/sql/00-initdb.sql
@@ -40,6 +40,19 @@ alter default privileges in schema realtor
     grant all privileges on tables to owner;
 
 -- tables
+drop table if exists realtor.location;
+create table realtor.location
+(
+    city          text   not null check (length(city) <= 63),
+    country       text   not null check (length(country) <= 63),
+    county        text check (length(county) <= 63),
+    district      text check (length(district) <= 63),
+    location_id   bigint not null generated always as identity primary key,
+    postal_code   text   not null check (length(postal_code) <= 15),
+    street_name   text   not null check (length(street_name) <= 63),
+    street_number text check (length(street_number) <= 15)
+);
+
 drop table if exists realtor.stats;
 create table realtor.stats
 (
@@ -50,31 +63,54 @@ create table realtor.stats
     updated_at  timestamp with time zone not null                          default now()
 );
 
+drop type if exists realtor.property_status;f
+create type realtor.property_status as enum (
+    'average',
+    'excellent',
+    'fair',
+    'good',
+    'poor'
+    );
+
 drop table if exists realtor.properties;
 create table realtor.properties
 (
-    property_id bigint not null generated always as identity primary key
+    location_id     bigint                  not null unique references realtor.location (location_id) on delete cascade on update cascade,
+    property_id     bigint                  not null generated always as identity primary key,
+    property_status realtor.property_status not null,
+    total_depth     bigint                  not null check (total_depth > 0),
+    total_height    bigint check (total_height > 0),
+    total_width     bigint                  not null check (total_width > 0)
 );
 
 drop table if exists realtor.apartments;
 create table realtor.apartments
 (
     apartment_id bigint not null generated always as identity primary key,
-    property_id  bigint not null unique references realtor.properties (property_id) on delete cascade on update cascade
+    floor        bigint not null check (floor > 0),
+    property_id  bigint not null unique references realtor.properties (property_id) on delete cascade on update cascade,
+    room_count   bigint not null check (room_count > 0)
 );
 
 drop table if exists realtor.houses;
 create table realtor.houses
 (
-    house_id    bigint not null generated always as identity primary key,
-    property_id bigint not null unique references realtor.properties (property_id) on delete cascade on update cascade
+    built_at     timestamp with time zone,
+    floor_count  bigint not null check (floor_count > 0),
+    has_basement boolean,
+    has_garage   boolean,
+    house_id     bigint not null generated always as identity primary key,
+    property_id  bigint not null unique references realtor.properties (property_id) on delete cascade on update cascade
 );
 
-drop table if exists realtor.offices;
-create table realtor.offices
+drop table if exists realtor.documents;
+create table realtor.documents
 (
-    office_id   bigint not null generated always as identity primary key,
-    property_id bigint not null unique references realtor.properties (property_id) on delete cascade on update cascade
+    data        bytea  not null,
+    document_id bigint not null generated always as identity primary key,
+    file_name   text   not null check (length(file_name) <= 255),
+    mime_type   text   not null check (length(mime_type) <= 63),
+    property_id bigint not null references realtor.properties (property_id) on delete cascade on update cascade
 );
 
 drop table if exists realtor.users;
