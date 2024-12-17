abeba51285179022708f4ef68f8508d574308052
fruzitent <fruzit@gmail.com>
2024-12-17 05:27:30 +0200

    feat(s3_dsa/p04): draw graph

--- s3_dsa/p04/src/bin/classic_bee/src/app.rs
+++ s3_dsa/p04/src/bin/classic_bee/src/app.rs
@@ -1,15 +1,19 @@
-pub struct App {}
+pub struct App {
+    g: egui_graphs::Graph,
+}
 
 impl App {
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self {}
+        Self {
+            g: egui_graphs::Graph::from(&petgraph::stable_graph::StableGraph::new()),
+        }
     }
 }
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Hello world!");
+            ui.add(&mut egui_graphs::GraphView::new(&mut self.g));
         });
     }
 }
