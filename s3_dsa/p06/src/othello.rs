ffd248889f1d55e9f1f633d981b2cc73ff28c319
fruzitent <fruzit@gmail.com>
2025-01-02 20:10:30 +0200

    refactor(s3_dsa/p06): rename structs

--- s3_dsa/p06/src/othello.rs
+++ s3_dsa/p06/src/othello.rs
@@ -1,41 +1,43 @@
-pub enum CellKind {
-    Black,
-    White,
+pub enum Side {
+    Dark,
+    Light,
 }
 
-#[derive(Default)]
-pub struct Cell {
-    pub kind: Option<CellKind>,
+pub struct Disk {
+    pub side: Side,
 }
 
-pub struct Board<Cell>
-where
-    Cell: Default,
-{
-    pub cells: Vec<Cell>,
+pub type Square = Option<Disk>;
+
+pub struct Board {
+    pub squares: Vec<Square>,
     pub stride: usize,
 }
 
-impl<Cell> Board<Cell>
-where
-    Cell: Default,
-{
+impl Board {
     pub fn new(stride: usize) -> Self {
         assert!(stride >= 4 && stride % 2 == 0);
-        Self {
-            cells: (0..stride.pow(2)).map(|_| Cell::default()).collect(),
-            stride,
-        }
+        let mut squares: Vec<_> = (0..stride.pow(2)).map(|_| None).collect();
+
+        let half_stride = stride / 2;
+        let top_left = (half_stride - 1) * stride + (half_stride - 1);
+        let top_right = (half_stride - 1) * stride + half_stride;
+        let bottom_left = half_stride * stride + (half_stride - 1);
+        let bottom_right = half_stride * stride + half_stride;
+
+        squares[top_left] = Some(Disk { side: Side::Light });
+        squares[top_right] = Some(Disk { side: Side::Dark });
+        squares[bottom_left] = Some(Disk { side: Side::Dark });
+        squares[bottom_right] = Some(Disk { side: Side::Light });
+
+        Self { squares, stride }
     }
 }
 
-impl<Cell> std::ops::Index<usize> for Board<Cell>
-where
-    Cell: Default,
-{
-    type Output = [Cell];
+impl std::ops::Index<usize> for Board {
+    type Output = [Square];
 
     fn index(&self, index: usize) -> &Self::Output {
-        &self.cells[index * self.stride..(index + 1) * self.stride]
+        &self.squares[index * self.stride..(index + 1) * self.stride]
     }
 }
--- s3_dsa/p06/src/othello_plugin.rs
+++ s3_dsa/p06/src/othello_plugin.rs
@@ -15,60 +15,80 @@ impl Plugin for OthelloPlugin {
 
 #[derive(Resource)]
 struct State {
-    board: othello::Board<othello::Cell>,
+    board: othello::Board,
 }
 
-fn setup(mut commands: Commands, mut state: ResMut<State>) {
-    let half_stride = state.board.stride / 2;
-    let top_left = (half_stride - 1) * state.board.stride + (half_stride - 1);
-    let top_right = (half_stride - 1) * state.board.stride + half_stride;
-    let bottom_left = half_stride * state.board.stride + (half_stride - 1);
-    let bottom_right = half_stride * state.board.stride + half_stride;
-    state.board.cells[top_left].kind = Some(othello::CellKind::White);
-    state.board.cells[top_right].kind = Some(othello::CellKind::Black);
-    state.board.cells[bottom_left].kind = Some(othello::CellKind::Black);
-    state.board.cells[bottom_right].kind = Some(othello::CellKind::White);
+fn setup(mut commands: Commands, state: Res<State>) {
     commands
-        .spawn(Node {
-            align_items: AlignItems::Center,
-            display: Display::Grid,
-            height: Val::Percent(100.0),
-            justify_items: JustifyItems::Center,
-            width: Val::Percent(100.0),
-            ..Default::default()
-        })
+        .spawn((
+            Name::new("App"),
+            Node {
+                align_items: AlignItems::Center,
+                display: Display::Grid,
+                height: Val::Percent(100.0),
+                justify_items: JustifyItems::Center,
+                width: Val::Percent(100.0),
+                ..Default::default()
+            },
+        ))
         .with_children(|builder| spawn_board(builder, &state));
 }
 
 fn spawn_board(builder: &mut ChildBuilder, state: &State) {
     builder
-        .spawn(Node {
-            aspect_ratio: Some(1.0),
-            column_gap: Val::Vh(1.0),
-            display: Display::Grid,
-            grid_template_columns: RepeatedGridTrack::flex(state.board.stride as u16, 1.0),
-            grid_template_rows: RepeatedGridTrack::flex(state.board.stride as u16, 1.0),
-            height: Val::Percent(80.0),
-            row_gap: Val::Vh(1.0),
-            ..Default::default()
-        })
-        .with_children(|builder| {
+        .spawn((
+            Name::new("Board"),
+            Node {
+                aspect_ratio: Some(1.0),
+                column_gap: Val::Vh(1.0),
+                display: Display::Grid,
+                grid_template_columns: RepeatedGridTrack::flex(state.board.stride as u16, 1.0),
+                grid_template_rows: RepeatedGridTrack::flex(state.board.stride as u16, 1.0),
+                height: Val::Percent(80.0),
+                row_gap: Val::Vh(1.0),
+                ..Default::default()
+            },
+        ))
+        .with_children(|parent| {
             for row in 0..state.board.stride {
                 for col in 0..state.board.stride {
-                    let index = row * state.board.stride + col;
-                    spawn_cell(builder, &state.board.cells[index]);
+                    let square = &state.board[row][col];
+                    spawn_square(parent, square);
                 }
             }
         });
 }
 
-fn spawn_cell(builder: &mut ChildBuilder, cell: &othello::Cell) {
+fn spawn_square(builder: &mut ChildBuilder, square: &othello::Square) {
+    builder
+        .spawn((
+            Name::new("Square"),
+            Node {
+                padding: UiRect::all(Val::Vh(0.5)),
+                ..Default::default()
+            },
+            BackgroundColor(Color::hsl(120.0, 0.5, 0.5)),
+        ))
+        .with_children(|parent| {
+            if let Some(disk) = square {
+                spawn_disk(parent, disk);
+            }
+        });
+}
+
+fn spawn_disk(builder: &mut ChildBuilder, disk: &othello::Disk) {
     builder.spawn((
-        Node::default(),
-        BackgroundColor(match cell.kind {
-            Some(othello::CellKind::Black) => Color::hsl(0.0, 0.0, 0.0),
-            Some(othello::CellKind::White) => Color::hsl(0.0, 1.0, 1.0),
-            None => Color::hsl(120.0, 0.5, 0.5),
+        Name::new("Disk"),
+        Node {
+            height: Val::Percent(100.0),
+            width: Val::Percent(100.0),
+            ..Default::default()
+        },
+        Button,
+        BackgroundColor(match disk.side {
+            othello::Side::Dark => Color::hsl(0.0, 0.0, 0.0),
+            othello::Side::Light => Color::hsl(0.0, 1.0, 1.0),
         }),
+        BorderRadius::all(Val::Percent(50.0)),
     ));
 }
