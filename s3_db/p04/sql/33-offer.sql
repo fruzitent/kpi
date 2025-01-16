fde5a23cd417c3da3495cce094c5b5dea0a519cf
fruzitent <fruzit@gmail.com>
2025-01-16 15:25:12 +0200

    fixup! feat(s3_db/p04): sql part 3

--- s3_db/p04/sql/33-offer.sql
+++ s3_db/p04/sql/33-offer.sql
@@ -1,15 +1,17 @@
-select *
-from (select distinct o.taker_id, count(*) over (partition by o.taker_id) as offer_count
-      from realtor.offer o
-               join realtor.stat s using (stat_id)
-      where o.offer_status = 'finished'
-        and s.updated_at > now() - interval '50y'
-      order by offer_count desc) as t
-where t.offer_count > 1;
---  taker_id | offer_count
--- ----------+-------------
---         7 |           2
---        11 |           2
---        30 |           2
---        35 |           2
+select o.taker_id,
+       count(*)           as offer_count,
+       sum(s.impressions) as sum_impressions
+from realtor.offer o
+         join realtor.stat s using (stat_id)
+where o.offer_status = 'finished'
+  and s.updated_at > now() - interval '50y'
+group by o.taker_id
+having count(*) > 1
+order by sum_impressions;
+--  taker_id | offer_count | sum_impressions
+-- ----------+-------------+-----------------
+--        11 |           2 |            3500
+--         7 |           2 |           16555
+--        30 |           2 |           23521
+--        35 |           2 |           56949
 -- (4 rows)
