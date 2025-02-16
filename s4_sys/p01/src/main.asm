5b196cca4ee0103e2e1e7417c62b80ec94548132
fruzitent <fruzit@gmail.com>
2025-02-16 17:39:55 +0200

    feat(s4_sys/p01): use linker script

--- s4_sys/p01/src/main.asm
+++ s4_sys/p01/src/main.asm
@@ -1,6 +1,4 @@
         bits 16
-; @see: https://ctyme.com/intr/rb-2270.htm
-; org 0x7c00
 
         section .text
         global _start
@@ -8,7 +6,3 @@
 _start:
         cli
         hlt
-
-; @see: https://github.com/microsoft/MS-DOS/blob/main/v4.0/src/BOOT/MSBOOT.ASM#L526
-        times ((512 -2) - ($ - $$)) db 0x00
-        dw 0xaa55
